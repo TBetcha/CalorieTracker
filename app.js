1c5bfd0f92f9da49039339d3f7f7ad4b03d4e967
Tbetcha <troy.boettger@gmail.com>
2020-06-01 13:31:20 -0400

    can now add items to list and list shows in UI

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const ItemCtrl = (function () {
 	};
 	//have to return to be able to publicly access
 	return {
+		getItems: function () {
+			return data.items;
+		},
 		logData: function () {
 			return data;
 		},
@@ -30,7 +33,28 @@ const ItemCtrl = (function () {
 })();
 
 //UI Controller
-const UICtrl = (function () {})();
+const UICtrl = (function () {
+	const UISelectors = {
+		itemList: '#item-list',
+	};
+
+	//public methods
+	return {
+		populateItemList: function (items) {
+			let html = '';
+			items.forEach(function (item) {
+				html += `      <li class="collection-item edit-item" id="item-${item.id}">
+				<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+				<a href="#" class="secondary-content">
+				  <i class="fa fa-pencil"></i>
+				</a>
+			 </li>`;
+			});
+			//Insert list items
+			document.querySelector(UISelectors.itemList).innerHTML = html;
+		},
+	};
+})();
 
 //App Controller
 const App = (function (ItemCtrl, UICtrl) {
@@ -38,6 +62,13 @@ const App = (function (ItemCtrl, UICtrl) {
 	return {
 		init: function () {
 			console.log('initializing app');
+
+			//fetch items from data structure
+			const items = ItemCtrl.getItems();
+			//populate list w/ items
+			UICtrl.populateItemList(items);
+
+			console.log(items);
 		},
 	};
 })(ItemCtrl, UICtrl);
